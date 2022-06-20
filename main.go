868c7d9bcc641c8bd3d5f3ee6f2dbeba1ef41be9
Alexis <perzmen@amazon.com>
2022-06-20 09:17:34 -0700

    Updated comments

--- main.go
+++ main.go
@@ -23,49 +23,52 @@ import (
 
 var meter = global.MeterProvider().Meter("app_or_package_name")
 var atomicClient atomic.Value
+
+//fallback client currently now used because otel will default to localhost:4318 on HTTP
 var fallbackClient = newClient(&DSN{
 	Original: "http://localhost:8000",
 	Host:     "localhost",
 	Port:     "8000",
 })
 
+// Structure to store custom DSN settings
 type DSN struct {
 	Original string
 	Port     string
 	Host     string
 }
 
+// Client struct that works compatible with otel sdk metric controller
 type client struct {
 	dsn  *DSN
 	ctrl *controller.Controller
 }
 
+// Dummy struct (probably not needed)
 type own struct {
 }
 
+// Function to join a host and a port
 func (dsn *DSN) AppAddr() string {
 	return "http://" + net.JoinHostPort(dsn.Host, dsn.Port)
 }
-func (dsn *DSN) OTLPHost() string {
-	if dsn.Host == "uptrace.dev" {
-		return "otlp.uptrace.dev:4317"
-	}
-	return dsn.Host
-}
 
+// Function that creates and returns a New client with certain options
+// In this case we are sending insecure options (http instead of https)
 func otlpmetricClient(dsn *DSN) otlpmetric.Client {
 	options := []otlpmetrichttp.Option{
-		// otlpmetrichttp.WithEndpoint(dsn.OTLPHost()),
 		otlpmetrichttp.WithInsecure(),
-		otlpmetrichttp.WithCompression(otlpmetrichttp.NoCompression),
+		// otlpmetrichttp.WithCompression(otlpmetrichttp.NoCompression),
 	}
 
 	return otlpmetrichttp.NewClient(options...)
 }
 
+// This function starts the client
 func configureMetrics(ctx context.Context, client *client) {
 	exportKindSelector := aggregation.StatelessTemporalitySelector()
 
+	// client.dsn is currently not being used. the default endpoint is being used (localhost:4318)
 	exp, err := otlpmetric.New(ctx, otlpmetricClient(client.dsn),
 		otlpmetric.WithMetricAggregationTemporalitySelector(exportKindSelector))
 	if err != nil {
@@ -82,6 +85,7 @@ func configureMetrics(ctx context.Context, client *client) {
 		controller.WithCollectPeriod(3*time.Second), // same as default
 	)
 
+	// Client is started here through the controller created above
 	if err := ctrl.Start(ctx); err != nil {
 		fmt.Printf("ctrl.Start failed: %s", err)
 		return
@@ -95,31 +99,28 @@ func configureMetrics(ctx context.Context, client *client) {
 }
 
 func main() {
-
-	// Utilizing metrictest
-	// Exporter being used here is not thread safe; Collect() has to be manually called
-	// TODO: Use OTLP stock exporter instead of the custom exporter found in metrictest
-
-	// otlpMetricWithCustomExporter()
-
+	// Start a context background and configure a new otel instance to being sending metrics to localhost:4318
 	ctx := context.Background()
 	own := newOwn()
-
 	own.configureOtel()
 
 	defer Shutdown(ctx)
-	// Synchronous instruments.
+	// Async create counter and counterobserver
 	go counter(ctx)
 	go counterObserver(ctx)
-	fmt.Println("reporting measurements to localhost... (press Ctrl+C to stop)")
+	fmt.Println("reporting measurements to localhost:4318... (press Ctrl+C to stop)")
 
 	ch := make(chan os.Signal, 3)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
 	<-ch
 }
+
+// Wrapper function to shutdown the active client
 func Shutdown(ctx context.Context) error {
 	return activeClient().Shutdown(ctx)
 }
+
+// Function to shutdown a specific client
 func (c *client) Shutdown(ctx context.Context) (lastErr error) {
 	if c.ctrl != nil {
 		if err := c.ctrl.Stop(ctx); err != nil {
@@ -128,6 +129,8 @@ func (c *client) Shutdown(ctx context.Context) (lastErr error) {
 	}
 	return lastErr
 }
+
+// Function that returns the active client stored in atomic client
 func activeClient() *client {
 	v := atomicClient.Load()
 	if v == nil {
@@ -136,6 +139,8 @@ func activeClient() *client {
 	return v.(*client)
 }
 
+// Function that sets a custom DSN and starts a new client with those DSN settings
+// Also starts the otel client to begin to send metrics generated
 func (own *own) configureOtel(opts ...instrument.Option) {
 	ctx := context.TODO()
 
@@ -149,16 +154,20 @@ func (own *own) configureOtel(opts ...instrument.Option) {
 	atomicClient.Store(client)
 
 }
+
+// Creates a new client given a dsn
 func newClient(dsn *DSN) *client {
 	return &client{
 		dsn: dsn,
 	}
 }
 
+// Returns a new dummy struct
 func newOwn() *own {
 	return &own{}
 }
 
+// Created a counter and adds 1 every second
 func counter(ctx context.Context) {
 	counter, _ := meter.SyncInt64().Counter(
 		"some.prefix.counter",
@@ -168,9 +177,10 @@ func counter(ctx context.Context) {
 
 	for {
 		counter.Add(ctx, 1)
-		time.Sleep(time.Millisecond)
+		time.Sleep(time.Second)
 	}
 }
+
 func counterObserver(ctx context.Context) {
 	counter, _ := meter.AsyncInt64().Counter(
 		"some.prefix.counter_observer",
