ce022c1161fd3367169fdb56b3c94c6c97bed085
Alexis <perzmen@amazon.com>
2022-06-20 08:36:16 -0700

    Working metric generator on default port 4318

--- main.go
+++ main.go
@@ -3,10 +3,96 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
+	"time"
 
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/sdk/metric/metrictest"
+	runtimemetrics "go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/metric/instrument"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
 )
+
+var meter = global.MeterProvider().Meter("app_or_package_name")
+var atomicClient atomic.Value
+var fallbackClient = newClient(&DSN{
+	Original: "http://localhost:8000",
+	Host:     "localhost",
+	Port:     "8000",
+})
+
+type DSN struct {
+	Original string
+	Port     string
+	Host     string
+}
+
+type client struct {
+	dsn  *DSN
+	ctrl *controller.Controller
+}
+
+type own struct {
+}
+
+func (dsn *DSN) AppAddr() string {
+	return "http://" + net.JoinHostPort(dsn.Host, dsn.Port)
+}
+func (dsn *DSN) OTLPHost() string {
+	if dsn.Host == "uptrace.dev" {
+		return "otlp.uptrace.dev:4317"
+	}
+	return dsn.Host
+}
+
+func otlpmetricClient(dsn *DSN) otlpmetric.Client {
+	options := []otlpmetrichttp.Option{
+		// otlpmetrichttp.WithEndpoint(dsn.OTLPHost()),
+		otlpmetrichttp.WithInsecure(),
+		otlpmetrichttp.WithCompression(otlpmetrichttp.NoCompression),
+	}
+
+	return otlpmetrichttp.NewClient(options...)
+}
+
+func configureMetrics(ctx context.Context, client *client) {
+	exportKindSelector := aggregation.StatelessTemporalitySelector()
+
+	exp, err := otlpmetric.New(ctx, otlpmetricClient(client.dsn),
+		otlpmetric.WithMetricAggregationTemporalitySelector(exportKindSelector))
+	if err != nil {
+		fmt.Printf("otlpmetric.New failed: %s", err)
+		return
+	}
+
+	ctrl := controller.New(
+		processor.NewFactory(
+			selector.NewWithHistogramDistribution(),
+			exportKindSelector,
+		),
+		controller.WithExporter(exp),
+		controller.WithCollectPeriod(3*time.Second), // same as default
+	)
+
+	if err := ctrl.Start(ctx); err != nil {
+		fmt.Printf("ctrl.Start failed: %s", err)
+		return
+	}
+	global.SetMeterProvider(ctrl)
+	client.ctrl = ctrl
+
+	if err := runtimemetrics.Start(); err != nil {
+		fmt.Printf("runtimemetrics.Start failed: %s", err)
+	}
+}
 
 func main() {
 
@@ -14,90 +100,95 @@ func main() {
 	// Exporter being used here is not thread safe; Collect() has to be manually called
 	// TODO: Use OTLP stock exporter instead of the custom exporter found in metrictest
 
-	otlpMetricWithCustomExporter()
+	// otlpMetricWithCustomExporter()
 
+	ctx := context.Background()
+	own := newOwn()
+
+	own.configureOtel()
+
+	defer Shutdown(ctx)
+	// Synchronous instruments.
+	go counter(ctx)
+	go counterObserver(ctx)
+	fmt.Println("reporting measurements to localhost... (press Ctrl+C to stop)")
+
+	ch := make(chan os.Signal, 3)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+	<-ch
+}
+func Shutdown(ctx context.Context) error {
+	return activeClient().Shutdown(ctx)
+}
+func (c *client) Shutdown(ctx context.Context) (lastErr error) {
+	if c.ctrl != nil {
+		if err := c.ctrl.Stop(ctx); err != nil {
+			lastErr = err
+		}
+	}
+	return lastErr
+}
+func activeClient() *client {
+	v := atomicClient.Load()
+	if v == nil {
+		return fallbackClient
+	}
+	return v.(*client)
 }
 
-func otlpMetricWithCustomExporter() {
-	ctx := context.Background()
-	mp, exp := metrictest.NewTestMeterProvider()
-	meter := mp.Meter("OTLP-Metrics")
+func (own *own) configureOtel(opts ...instrument.Option) {
+	ctx := context.TODO()
 
-	attrs := []attribute.KeyValue{attribute.Bool("test", true)}
-
-	//count
-	fcnt, err := meter.SyncInt64().Counter("iCount")
-
-	if err != nil {
-		fmt.Println(err)
+	dsn := &DSN{
+		Original: "http://localhost:8000",
+		Host:     "localhost",
+		Port:     "8000",
 	}
-	fcnt.Add(ctx, 2, attrs...)
-	fcnt.Add(ctx, 4, attrs...)
-	err = exp.Collect(context.Background())
+	client := newClient(dsn)
+	configureMetrics(ctx, client)
+	atomicClient.Store(client)
 
-	if err != nil {
-		fmt.Println(err)
+}
+func newClient(dsn *DSN) *client {
+	return &client{
+		dsn: dsn,
 	}
-	counterOut, err := exp.GetByName("iCount")
-	if err != nil {
-		fmt.Println(err)
-	}
+}
 
-	//UpDownCounter, Gauge
-	iudcnt, err := meter.SyncInt64().UpDownCounter("iUDCount")
-	if err != nil {
-		fmt.Println(err)
-	}
-	iudcnt.Add(ctx, 23, attrs...)
+func newOwn() *own {
+	return &own{}
+}
 
-	err = exp.Collect(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
-	gaugeOut, err := exp.GetByName("iUDCount")
+func counter(ctx context.Context) {
+	counter, _ := meter.SyncInt64().Counter(
+		"some.prefix.counter",
+		instrument.WithUnit("1"),
+		instrument.WithDescription("TODO"),
+	)
 
-	if err != nil {
-		fmt.Println(err)
+	for {
+		counter.Add(ctx, 1)
+		time.Sleep(time.Millisecond)
 	}
+}
+func counterObserver(ctx context.Context) {
+	counter, _ := meter.AsyncInt64().Counter(
+		"some.prefix.counter_observer",
+		instrument.WithUnit("1"),
+		instrument.WithDescription("TODO"),
+	)
 
-	// Histogram
-	ihis, err := meter.SyncInt64().Histogram("iHist")
-	if err != nil {
-		fmt.Println(err)
+	var number int64
+	if err := meter.RegisterCallback(
+		[]instrument.Asynchronous{
+			counter,
+		},
+		// SDK periodically calls this function to collect data.
+		func(ctx context.Context) {
+			number++
+			counter.Observe(ctx, number)
+		},
+	); err != nil {
+		panic(err)
 	}
-
-	ihis.Record(ctx, 24)
-	ihis.Record(ctx, 25)
-
-	err = exp.Collect(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
-	hisOut, err := exp.GetByName("iHist")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	ihis2, err := meter.SyncInt64().Histogram("iHist2")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	ihis2.Record(ctx, 24)
-	ihis2.Record(ctx, 25)
-	ihis2.Record(ctx, 26)
-
-	err = exp.Collect(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
-	hisOut2, err := exp.GetByName("iHist2")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(hisOut2)
-	// Printing out the metrics
-	fmt.Println(counterOut)
-	fmt.Println(gaugeOut)
-	fmt.Println(hisOut)
 }
